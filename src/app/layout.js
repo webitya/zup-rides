ecffbbea4183f59e44d359e306f1c0bb6a54f2be
adityawintech <aditya#19AI>
2025-05-02 19:33:15 +0530

    Aditya

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body>
         <Header />
         {children}
         <Footer />
