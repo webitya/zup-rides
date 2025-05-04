a3f3a4ae0855374ebf3bb86c479801891a47482c
adityawintech <aditya#19AI>
2025-05-04 06:43:07 +0530

    Aditya

--- src/Components/home_carousel/index.jsx
+++ src/Components/home_carousel/index.jsx
@@ -18,13 +18,13 @@ const slides = [
     id: 2,
     title: 'Escape into Nature',
     desc: 'Adventure-ready bikes for wild getaways.',
-    image: '/1.png',
+    image: '/pv.jpg',
   },
   {
     id: 3,
     title: 'Eco-Friendly Commuting',
     desc: 'Electric bikes to stay fast, fresh, and green.',
-    image: '/1.png',
+    image: '/cp.jpg',
   },
 ];
 
